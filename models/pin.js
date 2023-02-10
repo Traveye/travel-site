e42e0846f49e7b899aaac568e8454a3cbeeb2bdd
McCoy Didericksen <mccoy.did@gmail.com>
2023-02-10 15:11:32 -0700

    updating Pin model and adding seed command to package.json

--- models/pin.js
+++ models/pin.js
@@ -11,12 +11,8 @@ Pin.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        longitude: {
-            type: DataTypes.DECIMAL(10, 8), // first # is precision which is how many digits total, scale is how many digits after the decimal point - may need to adjust depending on how many digits we need
-            allowNull: false,
-        },
-        latitude: {
-            type: DataTypes.DECIMAL(10, 8),
+        coordinates: {
+            type: DataTypes.GEOMETRY('POINT'), // format is [longitude, latitude]]
             allowNull: false,
         },
         user_id: {
