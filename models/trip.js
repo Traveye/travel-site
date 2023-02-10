e9d1cb638f0428df56f8120e58647bb8ec1794f6
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-10 11:20:59 -0800

    added Trip Model and schema database

---
+++ models/trip.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_start: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    date_end: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    pin_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'pin',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'trip',
+  }
+);
+
+module.exports = Trip;
