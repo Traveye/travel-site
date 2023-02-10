155fe2f072bc9eda4152aaa4d585ad44bb0d9c8e
McCoy Didericksen <mccoy.did@gmail.com>
2023-02-10 12:58:05 -0700

    adding /dashboard route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const { User, Pin, Trip, Journal } = require('../models/index');
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
+    //TODO: add logic to check if user is logged in/redirect to dashboard if true
     res.render('login');
 });
+
+router.get('/dashboard', (req, res) => {
+    //TODO: add logic to check if user is logged in/redirect to login if false
+    //TODO: add logic to get user's pins, trips, and journals to pass to handlebars
+    res.render('dashboard');
+})
+
+module.exports = router;
