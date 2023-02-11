11a2c1329dab6834fb3767bfb468b0a1f8271c88
Big Trees <traveye@protonmail.com>
2023-02-11 14:01:20 -0800

    setting conditional for nav bar to not appear on the home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,13 +7,16 @@ router.get('/', (req, res) => {
         imagePath: '/images/dropin.PNG',
         imageAlt: 'Drop In logo',
       };
+
+    const showNav = false;
     res.render('login', logo);
 });
 
 router.get('/dashboard', (req, res) => {
     //TODO: add logic to check if user is logged in/redirect to login if false
     //TODO: add logic to get user's pins, trips, and journals to pass to handlebars
-    res.render('dashboard');
+    const showNav = true;
+    res.render('dashboard', showNav);
 })
 
 module.exports = router;
