8b2b100c66debdf4163d3e48525c0d7c90fad400
McCoy Didericksen <mccoy.did@gmail.com>
2023-02-11 11:03:40 -0700

    creating homeroute for pin view

--- controllers/api/tripRoutes.js
+++ controllers/api/tripRoutes.js
@@ -3,33 +3,6 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Pin, Trip, Journal} = require('../../models');
 
-
-// a route to get all trips is completed
-// :id is the pin id (when the user clicks on a pin)
-router.get('/:id', async (req, res) => {
-    try {
-        const tripData = await Pin.findAll({
-            where: {
-                id: req.params.id,
-            },
-            include: [
-                { 
-                    model: Trip,
-                    include: [
-                        {
-                            model: Journal,
-                        },
-                    ],
-                },
-            ],
-        });
-        const trips = tripData.map((trip) => trip.get({ plain: true }));
-        res.status(200).json(trips);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 // a route to add a new trip is completed
 router.post('/', async (req, res) => {
     try {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,6 +10,33 @@ router.get('/dashboard', (req, res) => {
     //TODO: add logic to check if user is logged in/redirect to login if false
     //TODO: add logic to get user's pins, trips, and journals to pass to handlebars
     res.render('dashboard');
-})
+});
+
+// :id is the pin id (when the user clicks on a pin)
+router.get('/pin/:id', async (req, res) => {
+    try {
+        const tripData = await Pin.findAll({
+            where: {
+                id: req.params.id,
+            },
+            include: [
+                { 
+                    model: Trip,
+                    include: [
+                        {
+                            model: Journal,
+                        },
+                    ],
+                },
+            ],
+        });
+        const trips = tripData.map((trip) => trip.get({ plain: true }));
+        res.render('pin', {
+            trips,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
