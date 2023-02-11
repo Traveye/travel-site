1bd547841377b14ff6e44bd83def15b3648859fe
Big Trees <traveye@protonmail.com>
2023-02-11 14:14:21 -0800

    fixing nav bug caused by multiple arguments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,25 @@
-const router = require('express').Router();
-const { User, Pin, Trip, Journal } = require('../models/index');
+const router = require("express").Router();
+const { User, Pin, Trip, Journal } = require("../models/index");
 
-router.get('/', (req, res) => {
-    //TODO: add logic to check if user is logged in/redirect to dashboard if true
-    const logo = {
-        imagePath: '/images/dropin.PNG',
-        imageAlt: 'Drop In logo',
-      };
+router.get("/", (req, res) => {
+  //TODO: add logic to check if user is logged in/redirect to dashboard if true
+  const data = {
+    logo: {
+      imagePath: "/images/dropin.PNG",
+      imageAlt: "Drop In logo",
+    },
+    showNav: false,
+  };
 
-    const showNav = false;
-    res.render('login', logo);
+  res.render("login", data);
 });
 
-router.get('/dashboard', (req, res) => {
-    //TODO: add logic to check if user is logged in/redirect to login if false
-    //TODO: add logic to get user's pins, trips, and journals to pass to handlebars
-    const showNav = true;
-    res.render('dashboard', showNav);
-})
+router.get("/dashboard", (req, res) => {
+  //TODO: add logic to check if user is logged in/redirect to login if false
+  //TODO: add logic to get user's pins, trips, and journals to pass to handlebars
+  const showNav = true;
+
+  res.render("dashboard", { showNav });
+});
 
 module.exports = router;
-
