68857f8825759569185984258bda421ae6f722fd
Big Trees <traveye@protonmail.com>
2023-02-12 00:31:55 -0800

    adding modal prelim styles

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,6 +1,7 @@
 // script to open / close the signup and login form
 
-const modal = document.querySelector("#modal");
+const modal = document.querySelector("#modal-1");
+const modal2 = document.querySelector("#modal-2");
 const openModal = document.querySelector(".open-button");
 const closeModal = document.querySelector(".close-button");
 
