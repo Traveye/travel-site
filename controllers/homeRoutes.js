05e7812ff95bc4d731d6b12d6e0dda96666f83d8
Big Trees <traveye@protonmail.com>
2023-02-11 20:02:19 -0800

    adding logo to navbar / homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,9 +17,15 @@ router.get("/", (req, res) => {
 router.get("/dashboard", (req, res) => {
     //TODO: add logic to check if user is logged in/redirect to login if false
     //TODO: add logic to get user's pins, trips, and journals to pass to handlebars
-    const showNav = true;
+    const data = {
+        logo: {
+            imagePath: "/images/dropin.PNG",
+            imageAlt: "Drop In logo" 
+        },
+        showNav: true,
+    }; 
   
-    res.render("dashboard", { showNav });
+    res.render("dashboard", data);
   });
 
 // :id is the pin id (when the user clicks on a pin)
@@ -84,6 +90,10 @@ router.get('/pin/:id', async (req, res) => {
         res.render('pin', {
             trips,
             showNav: true,
+            logo: {
+                imagePath: "/images/dropin.PNG",
+                imageAlt: "Drop In logo" 
+            }   
         });
     } catch (err) {
         res.status(500).json(err);
