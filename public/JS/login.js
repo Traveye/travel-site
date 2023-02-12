e679a75261cf5d970b5379bb56389e37fe235dfa
Big Trees <traveye@protonmail.com>
2023-02-11 23:45:56 -0800

    adding prelim modals

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,26 +1,9 @@
 // script to open / close the signup and login form
 
-const loginForm = document.getElementById('login-form');
-const signupForm = document.getElementById('signup-form');
-const loginBtn = document.getElementById('login-btn');
-const signupBtn = document.getElementById('signup-btn');
-const loginCloseBtn = document.getElementById('login-close-btn');
-const signupCloseBtn = document.getElementById('signup-close-btn');
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
 
-
-loginBtn.addEventListener('click', () => {
-    loginForm.style.display = 'block';
+openModal.addEventListener("click", () => {
+    modal.showModal();
 });
-
-signupBtn.addEventListener('click', () => {
-    signupForm.style.display = 'block';
-});
-
-loginCloseBtn.addEventListener('click', () => {
-    loginForm.style.display = 'none';
-});
-
-signupCloseBtn.addEventListener('click', () => {
-    signupForm.style.display = 'none';
-});
-
