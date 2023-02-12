a64b8a4add71f6c7f58d89a0f83546d3fd4b33c9
Big Trees <traveye@protonmail.com>
2023-02-12 02:24:51 -0800

    adding login function

--- public/JS/login.js
+++ public/JS/login.js
@@ -5,16 +5,40 @@ const modal2 = document.querySelector("#modal-2");
 const openModal1 = document.querySelector("#signup");
 const openModal2 = document.querySelector("#login");
 const closeModal = document.querySelector(".close-button");
+const signup = document.querySelector("#signup-call");
+const login = document.querySelector("#login-call");
 
 openModal1.addEventListener("click", () => {
-    modal1.showModal();
+  modal1.showModal();
 });
 
 openModal2.addEventListener("click", () => {
-    modal2.showModal();
+  modal2.showModal();
 });
 
 closeModal.addEventListener("click", () => {
-    modal1.close();
-    modal2.close();
+  modal1.close();
+  modal2.close();
+});
+
+//makes the fetch call to create a new user
+signup.addEventListener("click", async () => {
+  const username = document.querySelector("#username").value.trim();
+  const display_name = document.querySelector("#disply_name").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (username && display_name && password) {
+    try {
+      const response = await fetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({ username, display_name, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      console.log("Success:", data);
+      modal1.close();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 });
